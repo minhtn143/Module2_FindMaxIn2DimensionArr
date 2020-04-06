97d757ef8e35c2e5ac718bf971d98c5f3443ae94
minhtn143 <minhtn143@gmail.com>
2020-04-06 22:37:31 +0700

    upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>App Find Max in 2 Dimensional Array</title>
+</head>
+<body>
+<h2>App Find Max in 2 Dimensional Array</h2>
+    <form action="matrix.php" method="get">
+        <input type="number" name="rows">
+        <input type="number" name="cols">
+        <button type="submit">Create Matrix</button>
+    </form>
+
+</body>
+</html>
+
+
---
+++ matrix.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>App Find Max in 2 Dimensional Array</title>
+</head>
+<body>
+<h2>Matrix</h2>
+<form action="" method="post">
+    <table border="1">
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $rows = (int)$_REQUEST["rows"];
+            $cols = (int)$_REQUEST["cols"];
+            for ($i = 0; $i < $rows; $i++) {
+                echo '<tr>';
+                for ($j = 0; $j < $cols; $j++) {
+                    echo '<td>';
+                    echo '<input type="text" name="item[]">';
+                    echo ' </td>';
+                }
+                echo '</tr>';
+            }
+        }
+        ?>
+    </table>
+    <button type="submit">Find Max</button>
+</form>
+<?php
+    include "function.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $arrMatrix = $_REQUEST["item"];
+
+        function findMax($dataArr){
+            $length = count($dataArr);
+            $max= $dataArr[0];
+            for ($i=1;$i<$length;$i++){
+                if ($dataArr[$i]> $max){
+                    $max = $dataArr[$i];
+                }
+            }
+            return $max;
+        }
+
+        echo "Maximum is: ".findMax($arrMatrix);
+
+    }
+?>
+</body>
+</html>
+
+
